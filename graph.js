b10c1bac45ac7a8e02a02d09e955683f04d48db7
smartgmb0918 <vuehero18@outlook.com>
2019-12-12 02:47:15 -0800

    Add d3 legend

--- graph.js
+++ graph.js
@@ -19,10 +19,23 @@ const arcPath = d3.arc()
 
 const color = d3.scaleOrdinal(d3['schemeSet3']);
 
+// legend setup
+const legendGroup = svg.append('g')
+  .attr('transform', `translate(${dims.width + 40}, 10)`);
+
+const legend = d3.legendColor()
+  .shape('circle')
+  .shapePadding(10)
+  .scale(color);
+
 // update function
 const update = data => {
   // update color scale domain
   color.domain(data.map(d => d.name));
+
+  // update and call legend
+  legendGroup.call(legend);
+  legendGroup.selectAll('text').attr('fill', 'white');
 
   // join enhanced (pie) data to path elements
   const paths = graph.selectAll('path')
